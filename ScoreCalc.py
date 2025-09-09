83f21ccbb54c03dabafab4d908f172d481e9e171
qciosek <quincyciosek@gmail.com>
2025-09-09 16:24:47 -0400

    Update ScoreCalc.py

--- ScoreCalc.py
+++ ScoreCalc.py
@@ -23,7 +23,7 @@ def connect_to_db():
         pass  # Ignore error if no connection exists
 
     connection = pymysql.connect(
-        host= 'myinstancerestored.c5isyysu810z.us-east-2.rds.amazonaws.com',
+        host= 'database-1.c5isyysu810z.us-east-2.rds.amazonaws.com',
         user='admin',
         password='Omega1745!',
         database='study_data',
